fa8aafc86f6dcb780340ae424f5479f44b73190c
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-10-28 23:15:00 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grity::Application.config.secret_key_base = '48fd5da7d718541f6257b26e9ef85db2c370eb57ed442c575e641603096ca258ed5b364e553018093dfcf000a2a03a31c58fc31aa492abb963856d76b3ecfb83'
