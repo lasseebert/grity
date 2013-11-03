111a63debaa7fd9d716d4fd8cf99613933f0dc09
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-11-03 02:13:25 +0100

    Now tracks some stuff

--- app/assets/javascripts/anonymous_snippet.js.coffee
+++ app/assets/javascripts/anonymous_snippet.js.coffee
@@ -1,1 +1,3 @@
-console.log "Yo!"
+$ ->
+  url = window.location.origin + window.location.pathname + window.location.search
+  $.post('/api/pageviews', { session_id: "42", url: url })
--- spec/features/count_pageview_spec.rb
+++ spec/features/count_pageview_spec.rb
@@ -4,6 +4,15 @@ describe "javascript track pageview", js: true do
 
   it "tracks it" do
     visit '/'
+    # No workarounds found so far
+    sleep 0.1
     Pageview.count.should == 1
+    pageview = Pageview.all.first
+    pageview.user_agent.should be_present
+    pageview.session_id.should be_present
+  end
+
+  pending "different sessions" do
+    # Should give different session_ids
   end
 end
