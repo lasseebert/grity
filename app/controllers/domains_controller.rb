1a39522365ee41419ac589a66dfd0342ef0d8487
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-11-16 20:33:09 +0100

    Loads domain in DomainsController

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,6 +1,8 @@
 class DomainsController < ApplicationController
 
   def show
+    @domain = Domain.get params[:host]
+    raise Mongoid::Errors::DocumentNotFound, "No domain with host '#{params[:host]}'" unless @domain
   end
 
 end
